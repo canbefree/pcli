f6b0d44cc95354ab7ad37add1beaacc372b9b6fb
neo <452198757@qq.com>
2018-03-22 00:50:47 +0800

    console update (require phpunit)

---
+++ commands/TestCommand.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TestCommand {
+
+}
